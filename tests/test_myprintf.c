40063750de2b7217b229c0e515d2138e20496ab2
Herosbrine <gregoire.bezier@epitech.eu>
2019-11-12 20:59:43 +0100

    [ADD] | one criterion test

--- tests/test_myprintf.c
+++ tests/test_myprintf.c
@@ -40,3 +40,8 @@ Test(my_printf4, simple_string, .init = redirect_all_std)
     my_printf("hello world %2d et %2X et %2s et %2c et %2u et %2o et %2i", 14, 14, "14", "g", 4000000000, 14, 14);
     cr_assert_stdout_eq_str("hello world   14 et   E et   14 et  g et   4000000000 et   16 et   14");
 }
+Test(my_printf5, simple_string, .init = redirect_all_std)
+{
+    my_printf("hello world %   d et %   X et %    s et %    c et %   u et %    o et %   i", 14, 14, "14", "g", 4000000000, 14, 14);
+    cr_assert_stdout_eq_str("hello world  14 et  E et  14 et  g et  4000000000 et  16 et  14");
+}
